391bb4a30b3779460c725000ed0272568d49845e
Olaore Fouad <olaorefouad565@gmail.com>
2021-05-31 02:47:10 +0400

    feature: reviewed the flow for adding and displaying cart items, added the logger middleware to take care of all incoming requests, and worked on the file middleware returning the image if it exists and an error if it does not exist

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
-// static file middleware
-app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+    console.log('Incoming request to: ' + req.url);
+    next();
+})
 
-app.listen(3000)
+app.use((req, res, next) => {
+    var imagePath = path.join(__dirname, "static", req.url);
+    fs.stat(imagePath, (err, file) => {
+        if (err) {
+            next();
+            return;
+        } else if (file.isFile()) {
+            res.sendFile(imagePath);
+        } else {
+            next();
+        }
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("File Not Found!");
+})
+
+app.listen(3000, (_) => {
+    console.log('Server started!');
+})
