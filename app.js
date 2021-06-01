7a160d5a9be71c8247ec9e3be13c76cc9895eea9
Olaore Fouad <olaorefouad565@gmail.com>
2021-06-01 05:24:08 +0400

    feature: added support for mongodb and connected to eSchool database

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 
+// mongodb imports
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
+
+app.use((req, res, next) => {
+    MongoClient.connect(
+        "mongodb+srv://fouad:foodiepassword@cluster0.ev0uv.mongodb.net/eSchool"
+    ).then(client => {
+        req.lessonsCollection = client.db().collection("lessons");
+        req.ordersCollection = client.db().collection("orders");
+        console.log('Connected to database successfully!');
+        next()
+    }).catch(err => {
+        console.log('Error occurred while connecting to db!');
+    })
+})
 
 app.use((req, res, next) => {
     console.log('Incoming request to: ' + req.url);
