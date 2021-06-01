9a3f3139c3eb76474ffd504188ab30262a7780bf
Olaore Fouad <olaorefouad565@gmail.com>
2021-06-01 05:28:36 +0400

    feature: worked on a GET route that returns all lessons from the database

--- app.js
+++ app.js
@@ -7,43 +7,63 @@ const fs = require("fs");
 const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    MongoClient.connect(
-        "mongodb+srv://fouad:foodiepassword@cluster0.ev0uv.mongodb.net/eSchool"
-    ).then(client => {
-        req.lessonsCollection = client.db().collection("lessons");
-        req.ordersCollection = client.db().collection("orders");
-        console.log('Connected to database successfully!');
-        next()
-    }).catch(err => {
-        console.log('Error occurred while connecting to db!');
-    })
-})
-
-app.use((req, res, next) => {
-    console.log('Incoming request to: ' + req.url);
+    res.setHeader('Allow-Access-Control-Origin', '*');
     next();
 })
 
 app.use((req, res, next) => {
-    var imagePath = path.join(__dirname, "static", req.url);
-    fs.stat(imagePath, (err, file) => {
-        if (err) {
-            next();
-            return;
-        } else if (file.isFile()) {
-            res.sendFile(imagePath);
-        } else {
-            next();
-        }
+  MongoClient.connect(
+    "mongodb+srv://fouad:foodiepassword@cluster0.ev0uv.mongodb.net/eSchool"
+  )
+    .then((client) => {
+      req.lessonsCollection = client.db().collection("lessons");
+      req.ordersCollection = client.db().collection("orders");
+      console.log("Connected to database successfully!");
+      next();
     })
-})
+    .catch((err) => {
+      console.log("Error occurred while connecting to db!");
+    });
+});
 
 app.use((req, res, next) => {
-    res.status(404).send("File Not Found!");
-})
+  console.log("Incoming request to: " + req.url);
+  next();
+});
+
+app.get("/lessons", (req, res, next) => {
+  req.lessonsCollection
+    .find()
+    .toArray()
+    .then((lessons) => {
+      res.send(lessons);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use((req, res, next) => {
+  var imagePath = path.join(__dirname, "static", req.url);
+  fs.stat(imagePath, (err, file) => {
+    if (err) {
+      next();
+      return;
+    } else if (file.isFile()) {
+      res.sendFile(imagePath);
+    } else {
+      next();
+    }
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("File Not Found!");
+});
 
 app.listen(3000, (_) => {
-    console.log('Server started!');
-})
+  console.log("Server started!");
+});
